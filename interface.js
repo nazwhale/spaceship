b33cf248e7933316bcd9a0a934c4c5c71926fb3a
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-05-24 14:20:32 +0100

    extract changesky method to interface and add jasmine testing framework

---
+++ interface.js
@@ -0,0 +1,8 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//
+// });
+
+function changeSky(location) {
+  var sky = document.getElementById("image-360");
+  sky.setAttribute('src', location);
+}
