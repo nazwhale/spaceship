554761fea818e6bdec598b4740fa1c477b1c77d2
Ruan Odendaal <ruanodendaal@gmail.com>
2017-05-25 16:34:23 +0100

    ask for messaging permission

--- functions/config.js
+++ functions/config.js
@@ -8,3 +8,16 @@ var config = {
   messagingSenderId: "449052245084"
 };
 firebase.initializeApp(config);
+const messaging = firebase.messaging();
+console.log('RUN MEEEEE')
+messaging.requestPermission()
+.then(function(){
+  console.log('Have Permission');
+  return messaging.getToken();
+})
+.then(function(token) {
+  console.log(token);
+})
+.catch(function(err){
+  console.log('Error occured');
+})
--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,23 @@
 var functions = require('firebase-functions');
-var functions = require('./interface')
 
+// var functions = require('./interface');
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+const messaging = firebase.messaging();
+console.log('RUN MEEEEE')
+messaging.requestPermission()
+.then(function(){
+  console.log('Have Permission');
+  return messaging.getToken();
+})
+.then(function(token) {
+  console.log(token);
+})
+.catch(function(err){
+  console.log('Error occured');
+})
