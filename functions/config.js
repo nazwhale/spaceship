57c3f571fb09ae239a1077d978e233fc5a16320e
Ruan Odendaal <ruanodendaal@gmail.com>
2017-05-25 15:54:10 +0100

    add config

---
+++ functions/config.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCQMhU6yxjLCsanlSnUHtO4N6I6oCGlTI4",
+  authDomain: "spaceship-test.firebaseapp.com",
+  databaseURL: "https://spaceship-test.firebaseio.com",
+  projectId: "spaceship-test",
+  storageBucket: "spaceship-test.appspot.com",
+  messagingSenderId: "449052245084"
+};
+firebase.initializeApp(config);
