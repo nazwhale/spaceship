3f5a71bf0a112006c5da7b55bcda39c13b78091e
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-05-27 17:21:16 +0100

    write test for buildSpeechletResponseWithoutCard

--- spec/alexaSpec.js
+++ spec/alexaSpec.js
@@ -11,7 +11,7 @@ describe('#buildResponse', function() {
 
 describe('#buildSpeechletResponse', function() {
 
-  it('returns a hash with speech output', function() {
+  it('returns a hash with speech output and a card', function() {
     expect(buildSpeechletResponse('spaceship', 'this is mars', '', true)).toEqual({
         outputSpeech: {
             type: "PlainText",
@@ -21,6 +21,25 @@ describe('#buildSpeechletResponse', function() {
             type: "Simple",
             title: 'spaceship',
             content: 'this is mars'
+        },
+        reprompt: {
+            outputSpeech: {
+                type: "PlainText",
+                text: ''
+            }
+        },
+        shouldEndSession: true
+    });
+  });
+});
+
+describe('#buildSpeechletResponseWithoutCard', function() {
+
+  it('returns a hash with speech output', function() {
+    expect(buildSpeechletResponseWithoutCard('this is earth', '', true)).toEqual({
+        outputSpeech: {
+            type: "PlainText",
+            text: 'this is earth'
         },
         reprompt: {
             outputSpeech: {
