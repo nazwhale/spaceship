8b91227477f5ebed542c62c014ec3270f67c8afa
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-05-27 17:18:28 +0100

    add test for build speechlet response

--- spec/alexaSpec.js
+++ spec/alexaSpec.js
@@ -1,12 +1,34 @@
-describe('buildResponse', function() {
+describe('#buildResponse', function() {
 
   it('returns a response hash', function() {
     expect(buildResponse('session attributes', 'this is mars')).toEqual({
         version: "1.0",
         sessionAttributes: 'session attributes',
         response: 'this is mars'
-    })
+    });
   });
+});
 
+describe('#buildSpeechletResponse', function() {
 
+  it('returns a hash with speech output', function() {
+    expect(buildSpeechletResponse('spaceship', 'this is mars', '', true)).toEqual({
+        outputSpeech: {
+            type: "PlainText",
+            text: 'this is mars'
+        },
+        card: {
+            type: "Simple",
+            title: 'spaceship',
+            content: 'this is mars'
+        },
+        reprompt: {
+            outputSpeech: {
+                type: "PlainText",
+                text: ''
+            }
+        },
+        shouldEndSession: true
+    });
+  });
 });
