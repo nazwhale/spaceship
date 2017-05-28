08d9ba75bef5b5fdf43a36c48d5051479cd86a69
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-05-28 21:34:59 +0100

    some ideas for testing createSpy method

--- spec/aFrameSpec.js
+++ spec/aFrameSpec.js
@@ -1,2 +1,8 @@
-// changeSky('#earth')
-// expect(document.getElementById("image-360").getAttribute('src')).toEqual('#earth')
+// describe('#changeSky', function() {
+//   it('changes the 3D view', function() {
+//     var skyHtml = jasmine.createSpyObj('sky',['setAttribute']);
+//     spyOn(document, 'getElementById').and.returnValue(skyHtml);
+//     changeSky('earth');
+//     expect(skyHtml.setAtrribute).toHaveBeenCalledWith('src', 'earth');
+//   });
+// });
