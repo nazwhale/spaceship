442e2e29a047c1124505f35c1abaaacc294e62fe
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-05-30 17:27:54 +0100

    write and pass addMonolith function

--- public/js/interface.js
+++ public/js/interface.js
@@ -1,7 +1,22 @@
 (function(exports) {
+  
   function changeSky(location) {
     var sky = document.getElementById("image-360");
     sky.setAttribute('src', location);
   }
-  exports.changeSky = changeSky
+
+  function addMonolith() {
+    var box = document.createElement('a-box');
+    document.querySelector('a-scene').appendChild(box);
+    box.setAttribute('id', '#monolith');
+    box.setAttribute('color', '#222');
+    box.setAttribute('width', '0.5');
+    box.setAttribute('height', '4');
+    box.setAttribute('depth', '2');
+    box.setAttribute('position', '-5 2 0');
+    box.setAttribute('scale', '0.4 0.4 0.4');
+  }
+
+  exports.addMonolith = addMonolith;
+  exports.changeSky = changeSky;
 })(this);
--- spec/aFrameSpec.js
+++ spec/aFrameSpec.js
@@ -6,3 +6,13 @@ describe('#changeSky', function() {
     expect(dummyElement.getAttribute('src')).toEqual('#earth');
   });
 });
+
+describe('#addMonolith', function() {
+  it('adds a monolith to the view', function() {
+    var dummyElement = document.createElement('div');
+    spyOn(document, 'querySelector').and.returnValue(dummyElement);
+    addMonolith();
+    // expect(dummyElement.innerHTML).toEqual('<a-box id="#monolith" color="#222" width="0.5" height="4" depth="2" position="-5 2 0" scale="0.4 0.4 0.4"></a-box>');
+    expect(dummyElement.innerHTML).toContain('id="#monolith"');
+  });
+});
