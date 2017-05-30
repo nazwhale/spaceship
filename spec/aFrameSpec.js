f8a3e39a06405532d0ac8746b58953139b91be20
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-05-30 17:28:18 +0100

    remove comment

--- spec/aFrameSpec.js
+++ spec/aFrameSpec.js
@@ -12,7 +12,6 @@ describe('#addMonolith', function() {
     var dummyElement = document.createElement('div');
     spyOn(document, 'querySelector').and.returnValue(dummyElement);
     addMonolith();
-    // expect(dummyElement.innerHTML).toEqual('<a-box id="#monolith" color="#222" width="0.5" height="4" depth="2" position="-5 2 0" scale="0.4 0.4 0.4"></a-box>');
     expect(dummyElement.innerHTML).toContain('id="#monolith"');
   });
 });
