84b5b5d19d43c511e9431d440e493510f91397bf
Ruan Odendaal <ruanodendaal@gmail.com>
2017-05-31 11:03:18 +0100

    add ability to save browser token to firebase

--- public/js/interface.js
+++ public/js/interface.js
@@ -6,3 +6,27 @@
   }
   exports.changeSky = changeSky
 })(this);
+
+(function(exports) {
+
+  function captureToken(token) {
+    var database = firebase.database();
+    var browserTokens = database.ref('browserTokens')
+    var data = {
+      timestamp: Date.now(),
+      token: token
+    };
+    browserTokens.push(data, finished)
+  };
+
+  function finished(error) {
+    if (error) {
+      console.log('Did not save to DB' + error);
+    } else {
+      console.log('Browser token saved to DB');
+    }
+  }
+
+
+  exports.captureToken = captureToken
+})(this);
