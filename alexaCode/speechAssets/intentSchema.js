9d22147825f4d73050ffad4cd76155934bea0b87
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-05-31 16:55:51 +0100

    still working

--- alexaCode/speechAssets/intentSchema.js
+++ alexaCode/speechAssets/intentSchema.js
@@ -1,13 +1,28 @@
 {
   "intents": [
     {
-      "intent": "PlanetIntent",
       "slots": [
         {
           "name": "planet",
           "type": "LIST_OF_PLANETS"
         }
-      ]
+      ],
+      "intent": "PlanetIntent"
+    },
+    {
+      "intent": "RandomIntent"
+    },
+    {
+      "intent": "AMAZON.HelpIntent"
+    },
+    {
+      "slots": [
+        {
+          "name": "ufo",
+          "type": "LIST_OF_UFOS"
+        }
+      ],
+      "intent": "AddUfoIntent"
     }
   ]
 }
